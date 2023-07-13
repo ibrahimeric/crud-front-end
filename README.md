b31609aac96cec0b3245a628a15d539c28ba0a03
eric <ericibrahim19@gmail.com>
2023-07-13 11:26:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# crud-front-end
---
+++ js/main.js
@@ -0,0 +1,79 @@
+document.getElementById("header").innerHTML=`     <header>
+<nav class="header">
+    <div class="header_img">
+        <a title="Rhythm House" href="index.html"><img class="iconoLogo" src="img/freeLogo.png"
+                alt="Icono Rhythm House"></a>
+    </div>
+
+    <div class="header_links">
+        <a class="botonMenu" href="index.html">HOME</a>
+        <a class="botonMenu" href="producto_nuevo.html">NUEVOS PRODUCTOS</a>
+        <a class="botonMenu" href="producto_update.html">ACTUALIZAR PRODUCTOS</a>
+    </div>
+</nav>
+</header>`
+
+document.getElementById("footer").innerHTML=`   <footer>
+<div class="footer">
+    <div class="sb_footer section_padding">
+        <div class="sb_footer_links">
+            <div class="sb_footer_links_div">
+                <h4 class="footer_titulo">Info de la Web</h4>
+                <p>Sitio Web creado en el curso Full Stack Python. Codo a Codo año 2023.</p>
+            </div>
+            <div class="sb_footer_links_div">
+                <h4 class="footer_titulo">Desarrolladores</h4>
+                <ul class="footer_lista">
+                    <li class="integrantes">Devechi, Agustín</li>
+                    <li class="integrantes">Ibrahim, Eric</li>
+                    <li class="integrantes">Natucci, Valeria</li>
+                    <li class="integrantes">Nobile, Pablo</li>
+                </ul>
+            </div>
+            <div class="sb_footer_links_div">
+                <h4 class="footer_titulo">Contacto</h4>
+                <ul class='iconLista'>
+                    <li><a class='facebook' href="https://www.facebook.com/profile.php?id=100093154056964" target="_blank"><i
+                                class="fab fa-facebook-f fa-2x"></i></a></li>
+                    <li><a class='instagram' href="https://www.instagram.com/academiademusicarh/#" target="_blank"><i
+                                class="fab fa-instagram fa-2x"></i></a></li>
+                    <li><a class='twitter' href="https://twitter.com/RyhthmHouseAcad" target="_blank"><i class="fab fa-twitter fa-2x"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="sb_footer_below">
+            <div class="sb_footer_copyright">
+                <p>
+                    @2023 Rhythm House
+                </p>
+            </div>
+            <div class="sb_footer_below_links">
+                <a href="#">
+                    <div>
+                        <p>Terms & Conditions</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <p>Privacy</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <p>Security</p>
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <p>Cookie Declaration</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
+`
